74105b2ba449df9e032a9773d6dcb965a0e5f219
Davi Artur <daviartur2004@gmail.com>
2022-05-30 21:27:35 -0300

    adicionando interface store.tsx

--- src/helper/store.tsx
+++ src/helper/store.tsx
@@ -1,12 +1,16 @@
-import React, { ComponentElement, ProviderProps } from 'react'
+import React from 'react'
+import { ITask } from '../interfaces/Task'
 
+interface Context {
+  task?: any,
+  setTask?: any
+}
 
-export const GlobalStore = React.createContext<any>([]);
-
+export const GlobalStore = React.createContext<Context>({});
 
 export const UserContext = ({children}: any) => {
   
-  const [task, setTask] = React.useState<object>([]);
+  const [task, setTask] = React.useState<ITask[]>([]);
 
   return (
     <GlobalStore.Provider value={{
